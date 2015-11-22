c12247c2d380692aba83589a496e935dec39671c
johnpateha <Evgeny.Patekha@gmail.com>
2015-11-22 21:52:09 +0300

    Documentation update

--- Readme.md
+++ Readme.md
@@ -1,1 +1,14 @@
 Project description
+
+The body mass index (BMI) is defined as the body mass divided by the square of the body height, 
+and is universally expressed in units of kg/m2, resulting from mass in kilograms and height in metres. 
+
+BMI is commonly employed among children and adults to predict health outcomes. 
+Commonly accepted BMI ranges are underweight: under 18.5, normal weight: 18.5 to 25, overweight: 25 to 30, obese: over 30.
+
+For more details see http://en.wikipedia.org/wiki/Body_mass_index
+
+The app also uses "mixsmsn" R package, which contains BMI data for 2107 people. 
+This data reflect some sample distribution BMI values
+
+For more details see http://cran.r-project.org/web/packages/mixsmsn/mixsmsn.pdf
--- ui.R
+++ ui.R
@@ -4,17 +4,17 @@ shinyUI(fluidPage(
         sidebarLayout(
                 sidebarPanel(
                         radioButtons("units", label = h4("Units"),
-                                     choices = list("cm / kg" = 1, "inches / pounds" = 2), 
+                                     choices = list("cm / kg" = 1, "inches / lb" = 2), 
                                      selected = 1),
                         numericInput("height", label = h4("Your height ( cm / inches )"), 0, min=0),
-                        numericInput("weight", label = h4("Your weight ( kg / pounds )"), 0, min=0),
+                        numericInput("weight", label = h4("Your weight ( kg / lb )"), 0, min=0),
                         submitButton("Calc"),
                         hr(),
                         h4("Your BMI"),
                         verbatimTextOutput("resBMI"),
                         hr(),
                         helpText("Note: Choose unit system.",
-                                "Input your height (cm or inches) and weight (kg or pounds).",
+                                "Input your height (cm or inches) and weight (kg or lb).",
                                 "And push 'Calc' button."),
                         helpText("The app will calc Body Mass Index (BMI).",
                                 "At the histogram your BMI marked by blue line.")
